cab4250c7bed807e50fe0b1cf3b4d53f29adffe5
Thomaz Leite <i@thomazleite.com>
2014-12-22 07:15:00 -0800

    Fix spec_helper deprecation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
@@ -13,5 +12,5 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = 'random'
+  config.order = "random"
 end
